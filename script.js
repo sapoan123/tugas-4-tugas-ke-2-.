3124e34bc328946ca20a4a9aab3c737b845baa4d
sapoan <sapoan.anwar@students.amikom.ac.id>
2025-04-21 05:56:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let cheese = "cheddar"; // Ganti nilainya untuk tes kondisi lain
+
+if (cheese === "cheddar") {
+    console.log('Yay! Cheese available for making cheese on toast.');
+} else {
+    console.log('No cheese on toast for you today.');
+}
